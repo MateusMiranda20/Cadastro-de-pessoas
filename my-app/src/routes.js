0830aafa0b525e5b742fbf746ba0a727ab62e223
MateusMiranda20 <mateuusmiranda20@gmail.com>
2023-08-03 23:28:57 -0300

    Criando a Routes

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -1,1 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import Home from "./containers/Home/App";
+import Users from "./containers/Users/App";
+
+function MyRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/usuarios" Component={Users} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default MyRoutes;
